c9cc88854fb2c40593cda0fc50f92ad3fd8ca70e
Nick Palceski <nicholaspalceski@gmail.com>
2025-11-08 13:52:49 -0500

    Reverted changes of YT content to URL based search queries due to searching issues

--- voice-assistant-extention/youtubeContent.js
+++ voice-assistant-extention/youtubeContent.js
@@ -23,28 +23,8 @@ function scrollPage(command) {
 }
 
 async function searchYouTube(query) {
-  await waitFor(() => document.querySelector("input#search"));
-  const searchBox = document.querySelector("input#search");
-  if (!searchBox) return;
-
-  searchBox.focus();
-  searchBox.value = "";
-
-  // Human-like typing with React-compatible input events
-  for (let char of query) {
-    searchBox.value += char;
-    searchBox.dispatchEvent(new Event("input", { bubbles: true }));
-    await sleep(70 + Math.random() * 30);
-  }
-
-  const searchButton = document.querySelector(
-    "#search-icon-legacy button, ytd-searchbox #search-icon-legacy"
-  );
-  if (searchButton) {
-    searchButton.click();
-  } else {
-    console.warn("YouTube search button not found");
-  }
+  const searchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
+  chrome.tabs.create({ url: searchUrl });
 }
 
 
